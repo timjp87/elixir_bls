8ca417f7960e29e7987421547081bcc5ba23b36b
Tim Jester-Pfadt <tim.jester-pfadt@merckgroup.com>
2019-06-02 08:58:23 +0200

    Changed path from former umbrella app

--- mix.exs
+++ mix.exs
@@ -5,16 +5,26 @@ defmodule Bls.MixProject do
     [
       app: :bls,
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "../../config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
       elixir: "~> 1.8",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
       compilers: [:rustler] ++ Mix.compilers(),
       rustler_crates: rustler_crates(),
+      source_url: "https://github.com/timjp87/elixir-bls",
       test_coverage: [tool: ExCoveralls],
       deps: deps()
+    ]
+  end
+
+  defp description() do
+    "NIF wrapper around the Rust BLS 12-381 Elliptic Curve construction and signature aggregation scheme."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/timjp87/elixir-bls"}
     ]
   end
 
